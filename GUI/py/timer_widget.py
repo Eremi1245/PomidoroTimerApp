12964dcd523befa9bf2b4bd9dffd22d8d0037c9b
Eremi1245 <xxxmessixxx@mail.ru>
2022-05-06 10:38:56 +0300

    add button stop logick

--- GUI/py/timer_widget.py
+++ GUI/py/timer_widget.py
@@ -1,8 +1,7 @@
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtTest
+from GUI.utility.utility import sound_gong
 
-from GUI.utility.utility import timer
-
-timer_on = False
+timer_on = True
 
 
 class Ui_Form(object):
@@ -12,15 +11,15 @@ class Ui_Form(object):
         Form.setStyleSheet("background-color: rgb(255, 0, 128);")
         self.srt_btn = QtWidgets.QPushButton(Form)
         self.srt_btn.setGeometry(QtCore.QRect(20, 250, 150, 30))
-        self.srt_btn.setStyleSheet("background-color: rgb(85, 255, 0);")
+        self.srt_btn.setStyleSheet("background-color: rgb(128, 0, 128);")
         self.srt_btn.setObjectName("srt_btn")
         self.srt_btn.clicked.connect(self.click_start)
 
         self.stp_btn = QtWidgets.QPushButton(Form)
         self.stp_btn.setGeometry(QtCore.QRect(230, 250, 150, 30))
-        self.stp_btn.setStyleSheet("background-color: rgb(255, 0, 0);")
+        self.stp_btn.setStyleSheet("background-color: rgb(124,252,0);")
         self.stp_btn.setObjectName("stp_btn")
-        self.srt_btn.clicked.connect(self.click_stop)
+        self.stp_btn.clicked.connect(self.click_stop)
 
         self.lineEdit = QtWidgets.QLineEdit(Form)
         self.lineEdit.setGeometry(QtCore.QRect(10, 50, 381, 81))
@@ -35,21 +34,57 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Pimodoro Timer"))
-        self.srt_btn.setText(_translate("Form", "Start"))
-        self.stp_btn.setText(_translate("Form", "Stop"))
+        Form.setWindowTitle(_translate("Form", "Pomidoro Timer"))
+        self.srt_btn.setText(_translate("Form", "Start/Restart"))
+        self.stp_btn.setText(_translate("Form", "Pause/Continue"))
         self.lineEdit.setText(_translate("Form", "25:00"))
 
-    def click_start(self):
+    def click_start(self) -> None:
         """
-        Функция при нажатии на кнопку start. Запускает сначала таймер на работу 25 минут, потом таймер на отдых 5 минут
+        Функция при нажатии на кнопку start. Запускает сначала таймер на работу 25 минут,
+        потом таймер на отдых 5 минут
         """
-        timer(self.lineEdit, 25)
-        timer(self.lineEdit, 5)
+        self.timer(1)
+        self.timer(1)
 
     def click_stop(self):
         """
-        Функция при на нажатии на кнопку stop. Приостанавливает работу таймера
+        Функция при на нажатии на кнопку stop. Приостанавливает работу таймера и меняет цвет кнопки
         """
-        # TODO
-        pass
+        global timer_on
+        if timer_on:
+            timer_on = False
+            self.stp_btn.setStyleSheet("background-color: rgb(255,255,0);")
+        else:
+            timer_on = True
+            self.stp_btn.setStyleSheet("background-color: rgb(124,252,0);")
+
+    def timer(self, minutes: int) -> None:
+        """
+        Функция запускает таймер и отрисовывает текущее время на виджете
+        :param minutes: Количество минут таймера
+        """
+        global timer_on
+        time_in_seconds = minutes * 60
+        color_1 = 0
+        color_2 = 0
+        color_3 = 255
+        for i in range(time_in_seconds + 1):
+            if timer_on:
+                minutes = time_in_seconds // 60
+                seconds = time_in_seconds % 60
+                self.lineEdit.setText(f"{minutes}:{seconds}")
+                time_in_seconds -= 1
+                if color_1 < 255:
+                    color_1 += 1
+                else:
+                    if color_2 < 255:
+                        color_2 += 1
+                    else:
+                        if color_3 < 0:
+                            color_3 -= 1
+                QtTest.QTest.qWait(1000)
+            else:
+                while timer_on == False:
+                    QtTest.QTest.qWait(1000)
+        sound_gong()
